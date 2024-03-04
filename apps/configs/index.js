d255ead68bcb423edca47ff546fe481a36160430
misdarmanto <misdarmanto1922@gmail.com>
2024-03-04 08:05:31 +0700

    _

--- apps/configs/index.js
+++ apps/configs/index.js
@@ -7,45 +7,72 @@ exports.CONFIG = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.CONFIG = {
-    appVersion: process.env.APP_VERSION,
-    appSemantic: process.env.APP_SEMANTIC,
-    appMode: process.env.APP_MODE ?? 'development',
-    appUrl: process.env.APP_URL,
-    env: process.env.APP_ENV,
-    port: process.env.APP_PORT ?? 8000,
-    log: process.env.APP_LOG === 'true',
+    appVersion: '1.1',
+    appSemantic: 'ss',
+    appMode: 'development',
+    appUrl: 'http://localhost',
+    env: 'local',
+    port: 8006,
+    log: 'true',
     ipBlackList: [],
     secret: {
-        keyEncryption: process.env.SECRET_KEY_ENCRYPTION,
-        passwordEncryption: process.env.SECRET_PASSWORD_ENCRYPTION,
-        pinEncryption: process.env.SECRET_PIN_ENCRYPTION,
-        token: process.env.TOKEN_SECRET
+        keyEncryption: 'qwerty',
+        passwordEncryption: 'qwerty',
+        pinEncryption: 'qwerty',
+        token: 'qwerty'
     },
-    maximumUploadFile: process.env.MAXIMUM_UPLOAD_FILE ?? 1024,
+    maximumUploadFile: 1024,
     dataBase: {
         development: {
-            username: process.env.DB_USER_NAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            host: process.env.DB_HOST,
-            dialect: process.env.DB_DIALECT,
-            logging: process.env.DB_LOG === 'true'
-        },
-        testing: {
-            username: process.env.DB_USER_NAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            host: process.env.DB_HOST,
-            dialect: process.env.DB_DIALECT,
-            logging: process.env.DB_LOG === 'true'
-        },
-        production: {
-            username: process.env.DB_USER_NAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            host: process.env.DB_HOST,
-            dialect: process.env.DB_DIALECT,
-            logging: process.env.DB_LOG === 'true'
+            username: 'root',
+            password: '',
+            database: 'kedaireka_db',
+            host: 'localhost',
+            dialect: 'mysql',
+            logging: 'true'
         }
     }
 };
+// export const CONFIG = {
+//   appVersion: process.env.APP_VERSION,
+//   appSemantic: process.env.APP_SEMANTIC,
+//   appMode: process.env.APP_MODE ?? 'development',
+//   appUrl: process.env.APP_URL,
+//   env: process.env.APP_ENV,
+//   port: process.env.APP_PORT ?? 8000,
+//   log: process.env.APP_LOG === 'true',
+//   ipBlackList: [],
+//   secret: {
+//     keyEncryption: process.env.SECRET_KEY_ENCRYPTION,
+//     passwordEncryption: process.env.SECRET_PASSWORD_ENCRYPTION,
+//     pinEncryption: process.env.SECRET_PIN_ENCRYPTION,
+//     token: process.env.TOKEN_SECRET
+//   },
+//   maximumUploadFile: process.env.MAXIMUM_UPLOAD_FILE ?? 1024,
+//   dataBase: {
+//     development: {
+//       username: process.env.DB_USER_NAME,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       host: process.env.DB_HOST,
+//       dialect: process.env.DB_DIALECT,
+//       logging: process.env.DB_LOG === 'true'
+//     },
+//     testing: {
+//       username: process.env.DB_USER_NAME,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       host: process.env.DB_HOST,
+//       dialect: process.env.DB_DIALECT,
+//       logging: process.env.DB_LOG === 'true'
+//     },
+//     production: {
+//       username: process.env.DB_USER_NAME,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       host: process.env.DB_HOST,
+//       dialect: process.env.DB_DIALECT,
+//       logging: process.env.DB_LOG === 'true'
+//     }
+//   }
+// }
